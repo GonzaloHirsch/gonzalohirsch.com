6d07ac9af4f510c5fe9d6566af632b80dab88594
GonzaloHirsch <hirschgonzalo@gmail.com>
2022-07-24 16:59:43 -0300

    fix: improvements to blog slugs in case of not found

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,91 +1,89 @@
 <template>
     <main class="text-typography_primary_light dark:text-typography_primary_dark">
-        <template v-if="error">
-            <SectionsError />
-        </template>
-        <template v-else>
-            <Section id="blog-title" type="header">
-                <div
-                    class="border-t-2 pt-8 border-typography_primary_light dark:border-typography_primary_dark flex flex-col md:flex-row items-center md:justify-between md:text-right mb-12 md:mb-8"
-                >
-                    <ol itemscope itemtype="https://schema.org/BreadcrumbList" class="blog-breadcrumb">
-                        <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                            <a itemprop="item" href="https://gonzalohirsch.com/"> <span itemprop="name">Home</span></a>
-                            <meta itemprop="position" content="1" />
-                        </li>
-                        <span>/</span>
-                        <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                            <a
-                                itemscope
-                                itemtype="https://schema.org/WebPage"
-                                itemprop="item"
-                                itemid="https://gonzalohirsch.com/blog"
-                                href="https://gonzalohirsch.com/blog"
-                            >
-                                <span itemprop="name">Blog</span></a
-                            >
-                            <meta itemprop="position" content="2" />
-                        </li>
-                        <span>/</span>
-                        <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                            <span itemprop="name">{{ data?.article?.headline }}</span>
-                            <meta itemprop="position" content="3" />
-                        </li>
-                    </ol>
-                    <span class="font-light text-typography_primary_light/75 dark:text-typography_primary_dark/75 mt-2 md:mt-0">{{
-                        $formatDate(data?.article?.date)
-                    }}</span>
-                </div>
-                <h1 class="blog-post-text font-bold mb-4 md:mb-6 text-h3 leading-h3 md:text-h1 md:leading-h1 text-center md:text-left">
-                    {{ data?.article?.headline }}
-                </h1>
-                <p class="blog-post-text mb-8 md:w-8/12 md:text-lg md:leading-lg text-center md:text-left">{{ data?.article?.excerpt }}</p>
-                <div
-                    class="border-b-2 pb-8 border-typography_primary_light dark:border-typography_primary_dark flex flex-col md:flex-row items-center md:justify-between mt-12 md:mt-4"
-                >
-                    <div class="flex flex-row items-center justify-center">
-                        <img
-                            :src="getImage(data?.article?.author)"
-                            :alt="`Image of ${data?.article?.author}`"
-                            class="w-8 h-8 object-cover rounded-full mr-2"
-                        />
-                        <span class="blog-post-text text-lg leading-lg font-light"
-                            >By
-                            <a class="hover:underline italic" :href="data?.article?.authorUrl" target="_blank" rel="noopener noreferrer">{{
-                                data?.article?.author
-                            }}</a></span
-                        >
-                    </div>
-                    <div class="mt-6 md:mt-0">
-                        <NavShareIcons :headline="data?.article?.headline" :excerpt="data?.article?.excerpt" :path="data?.article?._path" />
-                    </div>
-                </div>
-            </Section>
-            <Section id="main" class="!pt-0 relative grid grid-cols-10 gap-8 lg:gap-12">
-                <aside class="col-span-full md:col-span-3 md:hidden">
-                    <div class="blog-post-text blog-aside-wrapper mb-2">
-                        <BlogTableOfContents :links="data?.article?.body?.toc?.links" />
-                    </div>
-                </aside>
-                <article class="blog-post-text prose col-span-full md:col-span-7 relative">
-                    <span
-                        v-show="data?.article?.dateUpdated"
-                        class="italic absolute -top-8 text-sm leading-sm font-light text-typography_primary_light/75 dark:text-typography_primary_dark/75"
-                        >(Updated at: {{ $formatDate(data?.article?.dateUpdated) }})</span
+        <ContentDoc>
+            <template v-slot="{ doc }">
+                <Section id="blog-title" type="header">
+                    <div
+                        class="border-t-2 pt-8 border-typography_primary_light dark:border-typography_primary_dark flex flex-col md:flex-row items-center md:justify-between md:text-right mb-12 md:mb-8"
                     >
-                    <ContentDoc :path="path" class="blog-post-text blog-content" />
-                </article>
-                <aside class="col-span-full md:col-span-3 blog-aside h-fit">
-                    <div class="!hidden blog-aside-wrapper md:!flex mb-4">
-                        <BlogTableOfContents :links="data?.article?.body?.toc?.links" class="blog-post-text" />
+                        <ol itemscope itemtype="https://schema.org/BreadcrumbList" class="blog-breadcrumb">
+                            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                                <a itemprop="item" href="https://gonzalohirsch.com/"> <span itemprop="name">Home</span></a>
+                                <meta itemprop="position" content="1" />
+                            </li>
+                            <span>/</span>
+                            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                                <a
+                                    itemscope
+                                    itemtype="https://schema.org/WebPage"
+                                    itemprop="item"
+                                    itemid="https://gonzalohirsch.com/blog"
+                                    href="https://gonzalohirsch.com/blog"
+                                >
+                                    <span itemprop="name">Blog</span></a
+                                >
+                                <meta itemprop="position" content="2" />
+                            </li>
+                            <span>/</span>
+                            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                                <span itemprop="name">{{ doc.headline }}</span>
+                                <meta itemprop="position" content="3" />
+                            </li>
+                        </ol>
+                        <span class="font-light text-typography_primary_light/75 dark:text-typography_primary_dark/75 mt-2 md:mt-0">{{
+                            $formatDate(doc.date)
+                        }}</span>
                     </div>
-                    <div v-if="data?.surround?.filter((elem) => elem !== null)?.length > 0" class="blog-aside-wrapper">
-                        <BlogRelatedArticles :surround="data?.surround" class="blog-post-text" />
+                    <h1 class="blog-post-text font-bold mb-4 md:mb-6 text-h3 leading-h3 md:text-h1 md:leading-h1 text-center md:text-left">
+                        {{ doc.headline }}
+                    </h1>
+                    <p class="blog-post-text mb-8 md:w-8/12 md:text-lg md:leading-lg text-center md:text-left">{{ doc.excerpt }}</p>
+                    <div
+                        class="border-b-2 pb-8 border-typography_primary_light dark:border-typography_primary_dark flex flex-col md:flex-row items-center md:justify-between mt-12 md:mt-4"
+                    >
+                        <div class="flex flex-row items-center justify-center">
+                            <img :src="getImage(doc.author)" :alt="`Image of ${doc.author}`" class="w-8 h-8 object-cover rounded-full mr-2" />
+                            <span class="blog-post-text text-lg leading-lg font-light"
+                                >By
+                                <a class="hover:underline italic" :href="doc.authorUrl" target="_blank" rel="noopener noreferrer">{{
+                                    doc.author
+                                }}</a></span
+                            >
+                        </div>
+                        <div class="mt-6 md:mt-0">
+                            <NavShareIcons :headline="doc.headline" :excerpt="doc.excerpt" :path="doc._path" />
+                        </div>
                     </div>
-                </aside>
-            </Section>
-            <NavScrollTopIcon />
-        </template>
+                </Section>
+                <Section id="main" class="!pt-0 relative grid grid-cols-10 gap-8 lg:gap-12">
+                    <aside class="col-span-full md:col-span-3 md:hidden">
+                        <div class="blog-post-text blog-aside-wrapper mb-2">
+                            <BlogTableOfContents :links="doc.body?.toc?.links" />
+                        </div>
+                    </aside>
+                    <article class="blog-post-text prose col-span-full md:col-span-7 relative">
+                        <span
+                            v-show="doc.dateUpdated"
+                            class="italic absolute -top-8 text-sm leading-sm font-light text-typography_primary_light/75 dark:text-typography_primary_dark/75"
+                            >(Updated at: {{ $formatDate(doc.dateUpdated) }})</span
+                        >
+                        <ContentRenderer :value="doc" class="blog-post-text blog-content" />
+                    </article>
+                    <aside class="col-span-full md:col-span-3 blog-aside h-fit">
+                        <div class="!hidden blog-aside-wrapper md:!flex mb-4">
+                            <BlogTableOfContents :links="doc.body?.toc?.links" class="blog-post-text" />
+                        </div>
+                        <div v-if="data?.surround?.filter((elem) => elem !== null)?.length > 0" class="blog-aside-wrapper">
+                            <BlogRelatedArticles :surround="data?.surround" class="blog-post-text" />
+                        </div>
+                    </aside>
+                </Section>
+                <NavScrollTopIcon />
+            </template>
+            <template #not-found>
+                <SectionsError />
+            </template>
+        </ContentDoc>
     </main>
 </template>
 
