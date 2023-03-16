8d6b3d3860df1d47662490fd04ced4a1621ce5e6
GonzaloHirsch <hirschgonzalo@gmail.com>
2023-03-16 20:05:57 -0300

    fix: more mobile usability issues

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,6 @@
 <script setup lang="ts">
 // Add base styles to fool Google when it doesn't load my styles
 useHead({
-  style: [ { innerHTML: 'code {display: grid; overflow: hidden;} p, a, span, li, code {font-size: 18px;} .custom-icon {margin: 0.5rem;}' } ]
+  style: [ { innerHTML: 'code {display: grid; overflow: hidden;} p, a, span, li, code, figcaption {font-size: 24px;} .custom-icon {margin: 0.5rem;} a {margin: 0.5rem; display: block}' } ]
 })
 </script>
